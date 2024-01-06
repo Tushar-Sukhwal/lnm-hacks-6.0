97f9a84b910ff8c00baa924c9bb68ee366f521fe
Ayush Bansal <116891199+Bansalayush247@users.noreply.github.com>
2024-01-07 01:48:51 +0530

    hero

--- app/Hero/hero.tsx
+++ app/Hero/hero.tsx
@@ -31,6 +31,22 @@ const ParallaxSectionContent: React.FC = () => {
         <div ref={refLeft} className={styles.boxLeft}>
           LNMHACKS
         </div>
+<<<<<<< HEAD
+        </div>
+        <div ref={refRight} className={styles.boxRight}>
+        Hold onto your hats because the next spectacular event 
+        around the corner is making a comeback, and it's bigger 
+        and better than ever!
+        </div>
+        <div ref={refLink} className={styles.linkContainer}>
+        <Link href="https://dorahacks.io/hackathon/lnmhacks">
+          <FaArrowDown />
+          <h1 className="font-futura font-black">CLICK HERE TO REGISTER</h1>
+            <FaArrowDown />
+          </Link>
+        </div>
+=======
+>>>>>>> 7b4db6a9a6e0fb274e32950d4ca7643ebb3e31b4
       </div>
       <div ref={refRight} className={styles.boxRight}>
         The next game changer is back for an encore&#x21; Don&rsquo;t
